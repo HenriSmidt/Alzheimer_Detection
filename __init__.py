51bc1a7b657a2d4f0dde0b73d0127a39df186400
HenriSmidt <henri.smidt@t-online.de>
2024-05-10 21:32:57 +0100

    code for averaging slices for relevancy inspectation

---
+++ __init__.py
@@ -0,0 +1,2 @@
+# from .dataset import PreprocessDataset, MRIDataModule, MRIDataset
+# __all__ = (PreprocessDataset, MRIDataModule, MRIDataset)
--- dataset.py
+++ dataset.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import lightning.pytorch as pl
 from sklearn.model_selection import train_test_split
+from torchvision.io import read_image
 
 class MRIDataset(Dataset):
     def __init__(self, data, transform=None):
@@ -57,3 +58,16 @@ class MRIDataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False)
+    
+class PreprocessDataset(Dataset):
+    def __init__(self, paths):
+        self.paths = paths
+    
+    def __len__(self):
+        return len(self.paths)
+    
+    def __getitem__(self, idx):
+        path = self.paths[idx]
+        image = read_image(path)  # Load image as a tensor
+        image = transforms.Grayscale()(image) / 255.0  # Convert to grayscale and normalize
+        return image
