f71c51c2994df5367e43317941810bb0de34558b
HenriSmidt <henri.smidt@t-online.de>
2024-05-29 18:41:46 +0100

    updating plots

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .login_credentials import LoginCredentials
-from .utils import get_best_device
-__all__ = (LoginCredentials, get_best_device)
+from .utils import get_best_device, set_plot_style
+__all__ = (LoginCredentials, get_best_device, set_plot_style)
