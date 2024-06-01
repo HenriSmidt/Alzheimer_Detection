3e601b88b731395857287f77b871f1083dfaec60
HenriSmidt <henri.smidt@t-online.de>
2024-06-01 18:28:18 +0100

    update init.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,7 @@
 from .efficientnet_baseline import EfficientNetBaseline
 from .mobilevit_lightning import MobileViTLightning
+from .model_factory import create_ensemble_model
+from .model_wrappers import GenericModelWrapper, EfficientNetWrapper, MobileViTWrapper
+from .ensemble_model import SimpleEnsembleModel, AdvancedEnsembleModel
 
-__all__ = (EfficientNetBaseline, MobileViTLightning)
+__all__ = (EfficientNetBaseline, MobileViTLightning, GenericModelWrapper, EfficientNetWrapper, MobileViTWrapper, SimpleEnsembleModel, AdvancedEnsembleModel, create_ensemble_model)
