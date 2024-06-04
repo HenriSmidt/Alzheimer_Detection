c81cec7245b757b178f4d96ada5efb1444474cd9
HenriSmidt <henri.smidt@t-online.de>
2024-06-04 17:03:12 +0100

    clean unnecessary code

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 from .efficientnet_baseline import EfficientNetBaseline
 from .mobilevit_lightning import MobileViTLightning
-from .model_factory import create_ensemble_model
-from .model_wrappers import ModelWrapper
 from .ensemble_model import SimpleEnsembleModel, AdvancedEnsembleModel
 
-__all__ = (EfficientNetBaseline, MobileViTLightning, ModelWrapper, SimpleEnsembleModel, AdvancedEnsembleModel, create_ensemble_model)
+__all__ = (EfficientNetBaseline, MobileViTLightning, SimpleEnsembleModel, AdvancedEnsembleModel)
--- models/model_factory.py
+++
@@ -1,9 +0,0 @@
-from .model_wrappers import ModelWrapper
-from .ensemble_model import SimpleEnsembleModel, AdvancedEnsembleModel
-
-def create_ensemble_model(model_wrappers, num_classes, use_advanced=False):
-    wrapped_models = [ModelWrapper(model) for model in model_wrappers]
-    if use_advanced:
-        return AdvancedEnsembleModel(wrapped_models, num_classes)
-    else:
-        return SimpleEnsembleModel(wrapped_models, num_classes)
